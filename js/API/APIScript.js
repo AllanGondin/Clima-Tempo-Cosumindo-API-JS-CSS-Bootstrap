4695294bba54a3865c92d9e04fe0640a80e92635
Allan <oliveiraallan@google.com>
2020-04-21 20:00:39 -0300

    Arrumdo divs menu

--- js/API/APIScript.js
+++ js/API/APIScript.js
@@ -1,7 +1,7 @@
-document.getElementById('busca').addEventListener('click', function(){
+document.querySelector("#busca").addEventListener('click', function(){
     var cidade = document.getElementById('buscaCidade').value;
   
-      const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=e76d33bb &city_name='+cidade
+      const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=e76d33bb&city_name='+cidade
     
       async function getWeather(){
       const response = await fetch(url_api);
