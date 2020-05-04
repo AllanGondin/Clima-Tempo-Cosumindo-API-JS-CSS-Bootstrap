b8687a0db16d76a1af9f9a80edf489dadb83795d
NickolasMoreira <nickolas.tato@gmail.com>
2020-05-03 23:37:44 -0300

    Acréscimo icone sol e aviao

--- js/API/APIScript.js
+++ js/API/APIScript.js
@@ -17,19 +17,30 @@ function queryString(parameter) {
 }
 
 var variavel = queryString("nomeCidade");
-console.log(variavel);
 
-    const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=367f6284&city_name='+variavel
+
+    const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=cab78132&city_name='+variavel;
 
       async function getWeather(){
       const response = await fetch(url_api);
       const data = await response.json();
       const search = data.by = "city_name";
 
-        document.getElementById('cidade').textContent = data.results.city_name;
-        document.getElementById('tempo').textContent = data.results.temp;
-        document.getElementById('data').textContent = data.results.date;
-
+     
+      
+      document.getElementById('cidade').textContent = data.results.city_name;
+      document.getElementById('temperatura').textContent = data.results.temp;
+      document.getElementById('data').textContent = data.results.time;
+      document.getElementById('minima').textContent = "";
+      document.getElementById('maxima').textContent = 20;
+      
+        
+        
+        
+     
+        
+        
+        
         var roupa = data.results.temp;
         if(roupa<20){
           console.log('Saia de blusa')
@@ -56,42 +67,3 @@ getWeather();
 
 
 
-/*
-document.querySelector("#busca").addEventListener('click', function(){
-    var cidade = document.getElementById('buscaCidade').value;
-    window.location.assign('file:///C:/Users/MOREIRA/Desktop/PI/NAFA/Cidade.html');
-    const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=367f6284&city_name='+cidade
-
-      async function getWeather(){
-      const response = await fetch(url_api);
-      const data = await response.json();
-      const search = data.by = "city_name";
-
-        document.getElementById('cidade').textContent = data.results.city_name;
-        document.getElementById('tempo').textContent = data.results.temp;
-        document.getElementById('data').textContent = data.results.date;
-
-        var roupa = data.results.temp;
-        if(roupa<20){
-          console.log('Saia de blusa')
-          document.getElementById('roupa').textContent = "Saia de blusa";
-          var sourceOfPicture = "Imagens/blusa.jpg";
-          var img = document.getElementById('bigpic')
-          img.src = sourceOfPicture;
-          img.style.display = "block";
-        }
-        else{
-          document.getElementById('roupa').textContent = "Saia de regata";
-          var sourceOfPicture = "Imagens/regata.jpg";
-          var img = document.getElementById('bigpic')
-          img.src = sourceOfPicture;
-          img.style.display = "block";
-        }
-        console.log(search)
-        console.log(data)
-        console.log(data.results.forecast[0].date);
-}
-
-getWeather();
-}); 
-*/
