a7d8e52271865576773e7521221772234a16dfd9
NickolasMoreira <nickolas.tato@gmail.com>
2020-05-04 21:13:35 -0300

    Hovers adicionados

--- js/indexT.js
+++ js/indexT.js
@@ -4,7 +4,8 @@ document.querySelector("#busca").addEventListener('click', function(){
 var passaValor= function(valor)
 {
    
-    window.location = "file:///C:/Users/allan/Projects/www/DW/NAFA/Cidade.html?nomeCidade="+valor;
+   // window.location = "file:///C:/Users/allan/Projects/www/DW/NAFA/Cidade.html?nomeCidade="+valor;
+    window.location ="file:///C:/Users/MOREIRA/Desktop/PI/NAFA/Cidade.html?nomeCidade="+valor;
 }
 
 
