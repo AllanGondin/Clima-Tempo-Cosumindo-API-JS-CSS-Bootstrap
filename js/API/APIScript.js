7ff6e16641ec681c1b98943d3f173eb8a75d1968
NickolasMoreira <nickolas.tato@gmail.com>
2020-05-18 18:52:16 -0300

    Terminar previsão próximos dias

--- js/API/APIScript.js
+++ js/API/APIScript.js
@@ -19,7 +19,7 @@ function queryString(parameter) {
 var variavel = queryString("nomeCidade");
 
 
-    const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=e76d33bb&city_name='+variavel;
+    const url_api = 'https://api.hgbrasil.com/weather?format=json-cors&key=367f6284&city_name='+variavel;
 
       async function getWeather(){
       const response = await fetch(url_api);
@@ -30,8 +30,34 @@ var variavel = queryString("nomeCidade");
       
       document.getElementById('cidade').textContent = data.results.city_name;
       document.getElementById('temperatura').textContent = data.results.temp + "°";
-      document.getElementById('minima').textContent = data.results.forecast[0].min + "°";
-      document.getElementById("maxima").textContent = data.results.forecast[0].max + "°";
+      document.getElementById('minima').textContent = data.results.forecast[0].min + "°" ; //DIA ATUAL
+      document.getElementById('maxima').textContent = data.results.forecast[0].max + "°";  //DIA ATUAL
+
+      //INFOS DIA + 1 
+      document.getElementById('wd1').textContent = data.results.forecast[1].weekday + " -"; 
+      document.getElementById('d1').textContent = data.results.forecast[1].date;
+      document.getElementById('mi1').textContent = data.results.forecast[1].min + "°" ;
+      document.getElementById('ma1').textContent = data.results.forecast[1].max + "°";  
+
+      //INFOS DIA + 2
+      document.getElementById('wd2').textContent = data.results.forecast[2].weekday + " -"; 
+      document.getElementById('d2').textContent = data.results.forecast[2].date;
+      document.getElementById('mi2').textContent = data.results.forecast[2].min + "°" ; 
+      document.getElementById('ma2').textContent = data.results.forecast[2].max + "°"; 
+
+      //INFOS DIA + 3
+      document.getElementById('wd3').textContent = data.results.forecast[3].weekday + " -"; 
+      document.getElementById('d3').textContent = data.results.forecast[3].date;
+      document.getElementById('mi3').textContent = data.results.forecast[3].min + "°" ; 
+      document.getElementById('ma3').textContent = data.results.forecast[3].max + "°";  
+
+      //INFOS DIA + 4
+      document.getElementById('wd4').textContent = data.results.forecast[4].weekday + " -"; 
+      document.getElementById('d4').textContent = data.results.forecast[4].date;
+      document.getElementById('mi4').textContent = data.results.forecast[4].min + "°" ; 
+      document.getElementById('ma4').textContent = data.results.forecast[4].max + "°";  
+      
+
       
         
         var roupa = data.results.temp;
